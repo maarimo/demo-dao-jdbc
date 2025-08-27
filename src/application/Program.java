395118fb7a803544535ac9142458bbe4f3e1700d
Matheus Amorim <165201036+maarimo@users.noreply.github.com>
2025-08-27 06:57:05 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,10 +35,17 @@ public class Program {
         };
 
         System.out.println();
-        System.out.println("=== TEST 4: seller findById ===");
+        System.out.println("=== TEST 4: seller insert ===");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
+        System.out.println();
+        System.out.println("=== TEST 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update Completed!");
+        
     }
 }
