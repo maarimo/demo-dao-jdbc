182d160f143803ff3decba41e458c290f3e49610
Matheus Amorim <165201036+maarimo@users.noreply.github.com>
2025-08-27 06:46:12 -0300

    Insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,5 +34,11 @@ public class Program {
             System.out.println(obj);
         };
 
+        System.out.println();
+        System.out.println("=== TEST 4: seller findById ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
+
     }
 }
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -7,9 +7,9 @@ import model.entities.Seller;
 import java.util.List;
 
 public interface SellerDao {
-    void insert(Seller obj);
-    void update(Seller obj);
-    void deleteById(Integer id);
+    void insert(Seller obj) throws DAOExceptions;
+    void update(Seller obj) throws DAOExceptions;
+    void deleteById(Integer id) throws DAOExceptions;
     Seller findById(Integer id) throws DAOExceptions;
     List<Seller> findAll() throws DAOExceptions;
     List<Seller> findByDepartment(Department department) throws DAOExceptions;
